844f8eedcf0ac43bc5251a811112bdc3324a1aeb
aagrosse <aagrock81@yahoo.com>
2020-04-16 18:22:52 -0400

    server edits

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
 const express = require("express");
-const logger = require("morgan");
 const mongoose = require("mongoose");
-const compression = require("compression");
 
-const PORT = process.env.PORT || 1980;
+const PORT = process.env.PORT || 1981;
 
 const app = express();
 
-app.use(logger("dev"));
-
-app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -20,7 +15,6 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useFindAndModify: false
 });
 
-// routes
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
