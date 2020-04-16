c3cf54a9877fc5ed16bc24254669ce00ca6bb9d3
aagrosse <aagrock81@yahoo.com>
2020-04-16 19:25:25 -0400

    db edits

--- public/db.js
+++ public/db.js
@@ -1,7 +1,9 @@
 let db;
+// create a new db request for a "budget" database.
 const request = indexedDB.open("budget", 1);
 
 request.onupgradeneeded = function (event) {
+  // create object store called "pending" and set autoIncrement to true
   const db = event.target.result;
   db.createObjectStore("pending", { autoIncrement: true });
 };
@@ -9,6 +11,7 @@ request.onupgradeneeded = function (event) {
 request.onsuccess = function (event) {
   db = event.target.result;
 
+  // check if app is online before reading from db
   if (navigator.onLine) {
     checkDatabase();
   }
@@ -19,15 +22,22 @@ request.onerror = function (event) {
 };
 
 function saveRecord(record) {
+  // create a transaction on the pending db with readwrite access
   const transaction = db.transaction(["pending"], "readwrite");
+
+  // access your pending object store
   const store = transaction.objectStore("pending");
 
+  // add record to your store with add method.
   store.add(record);
 }
 
 function checkDatabase() {
+  // open a transaction on your pending db
   const transaction = db.transaction(["pending"], "readwrite");
+  // access your pending object store
   const store = transaction.objectStore("pending");
+  // get all records from store and set to a variable
   const getAll = store.getAll();
 
   getAll.onsuccess = function () {
@@ -42,12 +52,18 @@ function checkDatabase() {
       })
         .then(response => response.json())
         .then(() => {
+          // if successful, open a transaction on your pending db
           const transaction = db.transaction(["pending"], "readwrite");
+
+          // access your pending object store
           const store = transaction.objectStore("pending");
+
+          // clear all items in your store
           store.clear();
         });
     }
   };
 }
 
+// listen for app coming back online
 window.addEventListener("online", checkDatabase);
--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require("express");
+const logger = require("morgan");
 const mongoose = require("mongoose");
-
-const PORT = process.env.PORT || 1981;
-
+const compression = require("compression");
+const PORT = process.env.PORT || 3000;
 const app = express();
 
+app.use(logger("dev"));
+app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://user1:password1@ds231725.mlab.com:31725/heroku_sqc6mlww", {
