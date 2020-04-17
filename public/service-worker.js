8e1dfd06ccc892351ac50b3260a4a74e13a63c17
aagrosse <aagrock81@yahoo.com>
2020-04-16 20:09:15 -0400

    added evt

--- public/service-worker.js
+++ public/service-worker.js
@@ -44,3 +44,11 @@ const FILES_TO_CACHE = [
       })
     );
   });
+
+  evt.respondWith(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.match(evt.request).then(response => {
+        return response || fetch(evt.request);
+      });
+    })
+  );  
